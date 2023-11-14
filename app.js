ffe866cd64ecffba82c69a373d9d0012cf3518f3
Marwa Oday <marwaoday@Marwas-MacBook-Air.local>
2023-11-14 10:38:04 +0000

    14 nov, you got this marwa alhamdulliah

--- app.js
+++ app.js
@@ -1,3 +1,111 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid')
+    let squares = Array.from(document.querySelectorAll('.grid div'))
+    const ScoreDisplay = document.querySelector('#score')
+    const StartBtn = document.querySelector('#start-button')
+    const width = 10;
+
+    const lTetromino =[
+        [1, width+1, width*2+1, 2],
+        [width, width+1, width+2, width*2+2],
+        [1, width+1, width*2+1, width*2],
+        [width, width*2, width*2+1, width*2+2]
+    ];
+
+    const zTetromino = [
+      [width+1, width+2, width*2, width*2+1],
+      [0, width, width+1, width*2+1],
+      [width+1, width+2, width*2, width*2+1],
+      [0, width, width+1, width*2+1]
+    ];
+
+    const tTetromino = [
+        [1, width, width+1, width+2],
+        [1, width+1, width+2, width*2+1],
+        [width, width+1, width+2, width*2+1],
+        [1, width, width+1, width+2+1]
+    ];
+
+    const oTetromino = [
+        [0, 1, width, width+1],
+        [0, 1, width, width+1],
+        [0, 1, width, width+1],
+        [0, 1, width, width+1]
+
+    ];
+
+    const iTetromino = [
+        [1, width+1, width*2+1, width*3+1],
+        [width, width+1, width+2, width+3],
+        [1, width+1, width*2+1, width*3+1],
+        [width, width+1, width+2, width+3]
+    ];
+
+    const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino ];
+
+    let currentPosition = 4;
+    let currentRotation = 0;
+    let random =Math.floor(Math.random() * theTetrominoes.length)
+    let current = theTetrominoes[random][currentRotation]
     
+
+    //drawing the Tetromino
+    function draw() {
+        current.forEach(index => {
+            squares[currentPosition + index].classList.add('tetromino')
+            
+        })
+    }
+
+    function undraw() {
+        current.forEach(index => {
+            squares[currentPosition + index].classList.remove('tetromino')
+        })
+    }
+
+timerId = setInterval(moveDown, 800)
+
+function control(e) {
+    if(e.keyCode === 37){
+        moveLeft()
+    } 
+}
+        
+document.addEventListener('keyup', control)
+
+
+        function moveDown() {
+            undraw()
+            currentPosition += width
+            draw()
+            freeze()
+        }
+        
+        //freeze
+        function freeze() {
+            if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
+                current.forEach(index => squares[currentPosition + index].classList.add('taken'))
+                //start a new tetrominoi to start falling
+                random = Math.floor(Math.random() * theTetrominoes.length)
+                current = theTetrominoes[random][currentRotation]
+                currentPosition = 4;
+                draw()
+            }
+        }
+        
+        
+        //move tetromino left, unless it is at the edge or there is a blockage
+        
+function moveLeft() { 
+
+
+}
+
+
+
+
+
+
+
+
 })
